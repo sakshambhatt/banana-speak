c46201668bf93958972d82985c83c86f3eda36d7
Saksham Bhatt <sakshambhatt33@gmail.com>
2020-12-17 11:29:49 +0530

    feat: input & output

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-
+var outputField = document.querySelector("#output");
 btnTranslate.addEventListener("click", function clickHandler() {
 
     console.log("clicked!");
     console.log("Your input: " + txtInput.value);
+    outputField.innerText = "Your input: " + txtInput.value;
 });
+
+
