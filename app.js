b032d503b5794e8864b8e062ebe267d297c81ebb
Saksham Bhatt <sakshambhatt33@gmail.com>
2020-12-19 21:02:49 +0530

    feat: translation using api

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputField = document.querySelector("#output");
+
+//function to construct a GET request
+function urlConstructor(text) {
+    return "https://api.funtranslations.com/translate/minion.json?text=" + text;
+}
+
+//function which performs fetch call
+function performFetch(text) {
+
+    fetch(urlConstructor(text))
+    .then(function responseHandler(response) {return response.json()})
+    .then (function sendJSON(json){outputField.innerText =  json.contents.translated + " " + 
+    json.contents.translation})
+    .catch((error) => {
+        console.log(error);
+    });
+}
+
 btnTranslate.addEventListener("click", function clickHandler() {
     
-    outputField.innerText = "Your input: " +  txtInput.value;
+    performFetch(txtInput.value);
 });
